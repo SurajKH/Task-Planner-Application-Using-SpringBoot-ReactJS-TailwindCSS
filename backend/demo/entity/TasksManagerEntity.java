69b03c696b0be9b4aa1e02e26306d12735bed7ec
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-06-04 16:17:42 +0530

    Add files via upload

---
+++ backend/demo/entity/TasksManagerEntity.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+//we need to consider the Entity Definition.
+
+/*
+ @Entity
+ @AllArgsConstructor
+ @NoArgsConstructor
+ @Getter
+ @Setter
+ @Table(name="tasks_manager_table)
+ */
+public class TasksManagerEntity {
+
+	/*
+	    Considering the concept of Many to One Relationship.
+	 */
+}
